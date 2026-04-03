3f6fb4e1bfc08ea34e0cf1f74a7d9f4387f00074
Saurav1233 <sauravmishra662@gmail.com>
2026-04-03 11:01:37 +0530

    Add superuser creation to build script

--- trustbuilt_backend/build.sh
+++ trustbuilt_backend/build.sh
@@ -2,3 +2,16 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email='admin@trustbuilt.in').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@trustbuilt.in',
+        password='Admin@2026'
+    )
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
