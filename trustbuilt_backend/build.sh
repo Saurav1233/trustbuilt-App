4b01924b5cdf4ff446e22473c3c3c6fc9c3201b8
Saurav1233 <sauravmishra662@gmail.com>
2026-04-05 15:20:14 +0530

    Change build.sh

--- trustbuilt_backend/build.sh
+++ trustbuilt_backend/build.sh
@@ -9,7 +9,6 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-# Update whichever admin exists
 try:
     user = User.objects.get(username='admin')
     user.email = 'trustbuilt2026@gmail.com'
@@ -17,12 +16,12 @@ try:
     user.is_staff = True
     user.is_superuser = True
     user.save()
-    print('✅ Admin updated: trustbuilt2026@gmail.com / Admin@2026')
+    print('Admin updated: trustbuilt2026@gmail.com / Admin@2026')
 except User.DoesNotExist:
     User.objects.create_superuser(
         username='admin',
         email='trustbuilt2026@gmail.com',
         password='Admin@2026'
     )
-    print('✅ Admin created: trustbuilt2026@gmail.com / Admin@2026')
+    print('Admin created: trustbuilt2026@gmail.com / Admin@2026')
 "
